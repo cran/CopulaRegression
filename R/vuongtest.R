e0d119a2e72de0a3e5a4ec127c63c84c1b861ecb
Nicole Kraemer <kraemer@ma.tum.de>
2013-03-13 00:00:00 +0000

    version 0.1-3

--- R/vuongtest.R
+++ R/vuongtest.R
@@ -4,7 +4,7 @@ vuongtest<-function(model1,model2,selection="AIC"){
     m<-ll1-ll2
     t.stat<-sqrt(length(m))*mean(m)/sd(m)
     if (selection=="AIC"){
-	t.stat=t.stat -(model1$npar --model2$npar)
+	t.stat=t.stat -(model1$npar -model2$npar)
 	}
     if (selection=="BIC"){
 	n=length(ll1)
